b649cdecbc99ffcd16bb70d1d500b1516b3e6461
Dhruvi Ladvaiya <23cs01035@iitbbs.ac.in>
2025-06-29 01:30:07 +0530

    Fix:handle middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
+// middleware.ts
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    /*
+     * Protects these routes:
+     * Add `/api/liveblocks-auth` here if not already matched
+     */
+    "/((?!_next/image|_next/static|favicon.ico).*)",
+  ],
 };
